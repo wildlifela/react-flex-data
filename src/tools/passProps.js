9d5e083f8f432a5d6844a4b90ee48351fb4690f5
Pavel Zagoskin <d4rklit3@wildlife.la>
2016-06-10 14:57:54 -0700

    stop omitting children in passProps()

--- src/tools/passProps.js
+++ src/tools/passProps.js
@@ -3,6 +3,6 @@ import _omit from 'lodash/omit';
 
 export default function(children, props) {
     return Children.map(children, (child, i) => {
-        return cloneElement(child, _omit({...props, ...child.props, childIndex: i}, 'children'));
+        return cloneElement(child, {...props, ...child.props, childIndex: i});
     });
 }
