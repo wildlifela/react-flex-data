c0d2b18c4d0c0b8b385a46e2c95a004e6dece135
Pavel Zagoskin <d4rklit3@wildlife.la>
2016-06-10 15:41:46 -0700

    update passprops to make sure child is a component

--- src/tools/passProps.js
+++ src/tools/passProps.js
@@ -1,8 +1,16 @@
-import {cloneElement, Children} from 'react';
-import _omit from 'lodash/omit';
+import React, {cloneElement, Children} from 'react';
 
 export default function(children, props) {
-    return Children.map(children, (child, i) => {
-        return cloneElement(child, {...props, ...child.props, childIndex: i});
+
+    const proppedChildren = Children.map(children, (child, i) => {
+        if(React.isValidElement(child)) {
+            return cloneElement(child, {...props, ...child.props, childIndex: i});
+        }else{
+            return child;
+        }
+
+
     });
+
+    return proppedChildren;
 }
