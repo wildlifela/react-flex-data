11ce30a88620daf360d691dd4f6d37082483aefb
samhunta <sam@email.ly>
2017-10-31 17:03:30 -0700

    Drop es6 from index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { default as Table } from './components/Table';
+export { default as TableBody } from './components/TableBody';
+export { default as TableHeader } from './components/TableHeader';
+export { default as TableHeaderColumn } from './components/TableHeaderColumn';
+export { default as TableRow } from './components/TableRow';
+export { default as TableRowColumn } from './components/TableRowColumn';
